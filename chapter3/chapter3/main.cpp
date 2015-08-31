b18c82ab1092f292d448aaf8f6c49fd4e1fb7707
robcio89 <robcio89@gmail.com>
2015-08-31 23:01:40 +0200

    unprotected data

--- chapter3/chapter3/main.cpp
+++ chapter3/chapter3/main.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include <algorithm>
 #include <mutex>
+#include <string>
 
 std::list<int> some_list;
 std::mutex some_mutex;
@@ -19,6 +20,49 @@ bool list_contains(int val)
 	return std::find(some_list.begin(), some_list.end(), val) != some_list.end();
 }
 
+
+class some_data
+{
+	int a;
+	std::string b;
+
+public:
+	void do_something()
+	{
+		a = 100;
+		b = "blah";
+		std::cout << "a: " << a << ", b: " << b << std::endl;
+	}
+};
+
+class data_wrapper
+{
+	some_data data;
+	std::mutex m;
+
+public:
+	template<typename Function>
+	void process_data(Function func)
+	{
+		std::lock_guard<std::mutex> l(m);
+		func(data);
+	}
+};
+
+some_data* unprotected;
+data_wrapper x;
+
+void malicious_function(some_data& protected_data)
+{
+	unprotected = &protected_data;
+}
+
+void foo()
+{
+	x.process_data(malicious_function);
+	unprotected->do_something();
+}
+
 int main()
 {
 	some_list.push_back(2);
@@ -27,4 +71,6 @@ int main()
 	some_list.push_back(44);
 
 	std::cout << "Hello" << std::endl;
+
+	foo();
 }
