7dbca1ffa7c0c5c36d158591daea86920e7e89c3
robcio89 <robcio89@gmail.com>
2015-08-31 22:47:22 +0200

    simple mutex example

--- chapter3/chapter3/main.cpp
+++ chapter3/chapter3/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <list>
+#include <algorithm>
+#include <mutex>
+
+std::list<int> some_list;
+std::mutex some_mutex;
+
+void add_to_list(int val)
+{
+	std::lock_guard<std::mutex> guard(some_mutex);
+	some_list.push_back(val);
+}
+
+bool list_contains(int val)
+{
+	std::lock_guard<std::mutex> guard(some_mutex);
+
+	return std::find(some_list.begin(), some_list.end(), val) != some_list.end();
+}
 
 int main()
 {
+	some_list.push_back(2);
+	some_list.push_back(21);
+	some_list.push_back(342);
+
 	std::cout << "Hello" << std::endl;
 }
